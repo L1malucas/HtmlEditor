225955e68f5da8fccf5d8a004697a630985ec7fe
lucas <lucaslima.94@hotmail.com.br>
2023-05-26 01:24:36 -0300

    editor finalizado

---
+++ Editor.cs
@@ -0,0 +1,31 @@
+using System.Text;
+
+namespace HtmlEditor;
+
+public static class Editor
+{
+    public static void Show()
+    {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("MODO DE EDIÇÃO");
+        Console.WriteLine("================");
+        Start();
+    }
+
+    public static void Start()
+    {
+        var file = new StringBuilder();
+
+        do
+        {
+            file.Append(Console.ReadLine());
+            file.Append(Environment.NewLine);
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.WriteLine("=====================");
+        Viewer.Show(file.ToString());
+    }
+}
